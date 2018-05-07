4ca9f1b1fc6acc5db2ff08add66330db170b1969
Vanderlei <hdelei@gmail.com>
2018-05-06 22:32:31 -0300

    New class Icon and formatting

--- CarCare/Controller/DynamicButton.cs
+++ CarCare/Controller/DynamicButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,12 +13,15 @@ namespace CarCare.Controller
     class DynamicButton
     {
         List<Button> buttonList = new List<Button>();
-        Control[] controls;
-        Random random = new Random();
+        Control[] controls;        
 
         public List<Button> ButtonList { get => buttonList; }
         public Control[] Controls { get => controls; }
 
+        /// <summary>
+        /// Creates a buttons list
+        /// </summary>
+        /// <param name="dt">Vehicle DataTable</param>
         public void CreateButtons(DataTable dt)
         {
             List<string> fields = new List<string>();
@@ -30,7 +34,11 @@ namespace CarCare.Controller
             controls = ButtonList.ToArray();           
         }
 
-        public void CreateOneButton(List<string> btData)
+        /// <summary>
+        /// Generate one new button
+        /// </summary>
+        /// <param name="btData">String List containing the new Vehicle data</param>
+        public void CreateOneButton(List<string> vehicleData)
         {
             DataTable dt = new DataTable();            
             dt.Columns.Add("id");
@@ -40,33 +48,38 @@ namespace CarCare.Controller
             dt.Columns.Add("plate");
             dt.Columns.Add("year");
 
-            dt.Rows.Add(btData.ToArray());                       
+            dt.Rows.Add(vehicleData.ToArray());                       
 
             CreateButtons(dt);
         }
 
-
+        /// <summary>
+        /// Build a button with all required parameters
+        /// </summary>
+        /// <param name="vehicle">String List with Vehicle data</param>
+        /// <returns>a single formatted button</returns>
         private Button BuildButton(List<string> vehicle)
         {
-            Image img = BuildImage();
+            Icon.SetIcon(vehicle[1]);
+            Dictionary<string, object> dict = Icon.GetIcon();
+            Image img = BuildImage((Image)dict["icon"]);
+
+            Color textColor = Color.White;
+            Color backColor = (Color)dict["backColor"];
+            
             string btText = vehicle[2] + "\r\n" + vehicle[4];
-
-            Color brown = Color.FromArgb(28,26,26);
-            Color green = Color.FromArgb(59, 221, 162);
-
 
             Button bt = new Button
             {
                 FlatStyle = FlatStyle.Flat,
                 Image = img,
                 Text = btText,
-                ForeColor = brown,
+                ForeColor = textColor,
                 TextAlign = ContentAlignment.MiddleCenter,
                 TextImageRelation = TextImageRelation.ImageAboveText,
                 Width = img.Width + 20,
                 Height = img.Height,
-                BackColor = green
-
+                BackColor = backColor
             };
 
             bt.FlatAppearance.BorderSize = 0;
@@ -76,30 +89,25 @@ namespace CarCare.Controller
             bt.AccessibleName = vehicle[2] + vehicle[0];
             bt.Click += (s, e) =>
             {
+                //TODO: Create the method of the button here
                 MessageBox.Show(bt.Name, bt.AccessibleName);
-
             };
 
             return bt;
         }
 
-        private Image BuildImage()
-        {
-            Image[] ima = { Properties.Resources.car, Properties.Resources.moto };
-
-            Image img = ima[random.Next(0, 2)];
+        /// <summary>
+        /// Format the image for button
+        /// </summary>
+        /// <param name="img">Image type PNG</param>
+        /// <returns>returns a new resized image</returns>
+        private Image BuildImage(Image img)
+        {   
             Bitmap newImage = new Bitmap(img.Width / 2, img.Height / 2);
             using (var graphics = Graphics.FromImage(newImage))
                 graphics.DrawImage(img, 0, 0, newImage.Width / 2, newImage.Height / 2);
 
             return newImage;
-        }
-
-        private List<object> CustomizeButton()
-        {
-
-            //TODO: Criar a lista de cores, figuras e metodos do botão
-            return new List<object>();
-        }
+        }        
     }
 }
---
+++ CarCare/Controller/Icon.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarCare.Properties;
+
+namespace CarCare.Controller
+{
+    class Icon
+    {
+        static Dictionary<string, object> formattedIcon = new Dictionary<string, object>();
+
+        static Dictionary<string, Image> icons = new Dictionary<string, Image>()
+        {
+            { "carro", Resources.car },
+            { "moto", Resources.moto },
+            { "caminhão", Resources.truck },
+            { "balão", Resources.balloon },
+            { "submarino", Resources.submarine },
+            { "bicicleta", Resources.bike },
+            { "helicóptero", Resources.helicopter },
+            { "barco", Resources.boat }    
+        };
+
+        static Dictionary<string, Color> backColors = new Dictionary<string, Color>()
+        {
+            { "carro", Color.FromArgb(66,134,244) },
+            { "moto", Color.FromArgb(56,23,89) },
+            { "caminhão", Color.FromArgb(128,38,142) },
+            { "balão", Color.FromArgb(28,26,26) },
+            { "submarino", Color.FromArgb(244,150,40) },
+            { "bicicleta", Color.FromArgb(31,142,14) },
+            { "helicóptero", Color.FromArgb(181,39,27) },
+            { "barco", Color.FromArgb(175,59,59) }
+        };
+
+        static Dictionary<string, Color> fontColors = new Dictionary<string, Color>()
+        {
+            { "carro", Color.FromArgb(28,26,26) },
+            { "moto", Color.FromArgb(28,26,26) },
+            { "caminhão", Color.FromArgb(28,26,26) },
+            { "balão", Color.FromArgb(28,26,26) },
+            { "submarino", Color.FromArgb(28,26,26) },
+            { "bicicleta", Color.FromArgb(28,26,26) },
+            { "helicóptero", Color.FromArgb(28,26,26) },
+            { "barco", Color.FromArgb(28,26,26) }
+        };
+
+        public static void SetIcon(string vehicle)
+        {
+            formattedIcon.Clear();
+            formattedIcon.Add("icon", icons[vehicle]);
+            formattedIcon.Add("backColor", backColors[vehicle]);
+            formattedIcon.Add("fontColor", fontColors[vehicle]);
+        }
+
+        public static Dictionary<string, object> GetIcon()
+        {
+            return formattedIcon;
+        }
+    }
+}
